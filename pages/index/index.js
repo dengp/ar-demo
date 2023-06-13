e57664bfadbe0867e211837b5fedc9940a34f83f
邓平 <ping.deng@tencentmusic.com>
2023-06-13 14:47:15 +0800

    feat(init): ar

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,130 @@
 // index.js
-// 获取应用实例
-const app = getApp()
+import getBehavior from './behavior'
+import yuvBehavior from './yuvBehavior'
 
-Page({
+const NEAR = 0.001
+const FAR = 1000
+
+Component({
+  behaviors: [getBehavior(), yuvBehavior],
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    theme: 'light',
+    frameShow: false,
+    frameX: 0,
+    frameY: 0,
+    frameWidth: 0,
+    frameHeight: 0
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
+  lifetimes: {
+    detached() {
+      if (wx.offThemeChange) {
+        wx.offThemeChange()
+      }
+    },
+    ready() {
+      wx.showLoading()
       this.setData({
-        canIUseGetUserProfile: true
+        theme: wx.getSystemInfoSync().theme || 'light'
+      })
+      if (wx.onThemeChange) {
+        wx.onThemeChange(({theme}) => {
+          this.setData({theme})
+        })
+      }
+    },
+  },
+  methods: {
+    init() {
+      this.initGL()
+    },
+    afterVKSessionCreated() {
+      this.session.on('addAnchors', anchors => {
+        const anchor = anchors[0]
+        const {
+          width,
+          height
+        } = this.data
+        if (anchor && this.markerId) {
+          this.setData({
+            frameShow: true,
+            frameX: anchor.origin.x * width,
+            frameY: anchor.origin.y * height,
+            frameWidth: anchor.size.width * width,
+            frameHeight: anchor.size.height * height,
+          })
+        }
+      })
+      this.session.on('updateAnchors', anchors => {
+        const anchor = anchors[0]
+        const {
+          width,
+          height
+        } = this.data
+        if (anchor) {
+          this.setData({
+            frameX: anchor.origin.x * width,
+            frameY: anchor.origin.y * height,
+            frameWidth: anchor.size.width * width,
+            frameHeight: anchor.size.height * height,
+          })
+        }
+      })
+      this.session.on('removeAnchors', anchors => {
+        this.setData({
+          frameShow: false,
+        })
+      })
+      // this.addOSDMarker('dh')
+      this.addOSDMarker('jz')
+    },
+    render(frame) {
+      this.renderGL(frame)
+      const camera = frame.camera
+      if (camera) {
+        this.camera.matrixAutoUpdate = false
+        this.camera.matrixWorldInverse.fromArray(camera.viewMatrix)
+        this.camera.matrixWorld.getInverse(this.camera.matrixWorldInverse)
+        const projectionMatrix = camera.getProjectionMatrix(NEAR, FAR)
+        this.camera.projectionMatrix.fromArray(projectionMatrix)
+        this.camera.projectionMatrixInverse.getInverse(this.camera.projectionMatrix)
+      }
+      this.renderer.autoClearColor = false
+      this.renderer.render(this.scene, this.camera)
+      this.renderer.state.setCullFace(this.THREE.CullFaceNone)
+    },
+    addOSDMarker(fileName) {
+      // if (this.markerId) return
+      const fs = wx.getFileSystemManager()
+      const filePath = `${wx.env.USER_DATA_PATH}/osd-${fileName}.jpg`
+
+      wx.compressImage({
+        src: `./assets/${fileName}.png`, 
+        quality: 100,
+        success: (res) => {
+          fs.saveFile({
+            filePath,
+            tempFilePath: res.tempFilePath,
+            success: () => {
+              this.markerId = this.session.addOSDMarker(filePath)
+              wx.hideLoading()
+            }
+          })
+        }
+      })
+    },
+    resultClick() {
+      wx.showModal({
+        title: '提示',
+        content: '崇宁镇老爷庙是渭华起义发祥地，想听听渭华起义的更多历史吗?',
+        success (res) {
+          if (res.confirm) {
+            const innerAudioContext = wx.createInnerAudioContext()
+            innerAudioContext.autoplay = true
+            innerAudioContext.src = 'https://tqai-76286.gzc.vod.tencent-cloud.com/3024/bc269d073a33bed22a9070211a6b2850.mp3'
+          } else if (res.cancel) {
+          }
+        }
       })
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
